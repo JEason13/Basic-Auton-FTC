736ed1bf84e016579f1ba094a6182c722ed8d4a7
Jacob Eason <31490880+JEason13@users.noreply.github.com>
2018-11-05 20:35:25 -0500

    Update TestTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -1,1 +1,67 @@
+package org.firstinspires.ftc.teamcode;
 
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "TestTeleOp", group = "JacobE")
+public class TestTeleOp extends LinearOpMode
+{
+
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+
+
+    //public TeleOp10620() {
+
+    //}
+
+    @Override
+    public void runOpMode()
+    {
+        DcMotor motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        DcMotor motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+
+            double leftPower;
+            double rightPower;
+            double sanic;
+
+
+            double drive = gamepad1.left_stick_y * 0.6;
+            double turn  =  gamepad1.right_stick_x * 0.6;
+
+
+            sanic = 1.0;
+            leftPower    = drive + turn;
+            rightPower   = drive - turn;
+            double scale = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+
+            if (scale > 1) {
+                scale = 1 / scale;
+                leftPower *= scale;
+                rightPower *= scale;
+            }
+
+            motorLeft.setPower(leftPower);
+            motorRight.setPower(rightPower);
+            if (gamepad1.a){
+                motorLeft.setPower(sanic);
+                motorRight.setPower(sanic);
+            }
+
+            idle();
+
+        }
+
+    }
+}
